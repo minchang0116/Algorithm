f1482d0ab21e2785724b5cdeb89fce40f8506136
DESKTOP-PDHTUTV\new08 <new0822@naver.com>
2021-01-10 23:14:33 +0900

    프로그래머스

---
+++ src/PROG/lv4/징검다리.java
@@ -0,0 +1,12 @@
+package PROG.lv4;
+
+public class 징검다리 {
+	public static void main(String[] args) {
+		징검다리 service = new 징검다리();
+		System.out.println(service.solution(25, new int[] {2, 14,11,21,17},2));
+	}
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
