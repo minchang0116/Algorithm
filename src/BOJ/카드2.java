3040e9bfd6a019c73be94047cd6678db69e3bdb3
강민창 <new0822@naver.com>
2021-06-10 17:26:23 +0900

    백준 문제풀이 2

---
+++ src/BOJ/카드2.java
@@ -0,0 +1,27 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 카드2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<Integer>();
+		for(int i=1; i<=N; i++) {
+			q.offer(i);
+		}
+		while(q.size() != 1) {
+			q.poll();
+			if(q.size() == 1) {
+				break;
+			}
+			int value = q.poll();
+			q.offer(value);
+		}
+		System.out.println(q.peek());
+	}
+}
