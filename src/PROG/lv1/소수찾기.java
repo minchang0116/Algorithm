680938c125e985b9c7b846b7a4d87b60516af689
강민창 <new0822@naver.com>
2021-06-28 23:54:17 +0900

    프로그래머스 문제풀이

--- src/PROG/lv1/소수찾기.java
+++ src/PROG/lv1/소수찾기.java
@@ -4,4 +4,16 @@ public class 소수찾기 {
 	public static void main(String[] args) {
 		
 	}
+    public int solution(int n) {
+        int answer = 0;
+        outer: for(int i=2; i<=n; i++){
+            for(int j=2; j<=Math.sqrt(i); j++){
+                if(i % j == 0){
+                    continue outer;
+                }
+            }
+            answer++;
+        }
+        return answer;
+    }
 }
